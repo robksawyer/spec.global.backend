d9e64704ba3ee308e76f050a59f2121fb6b4d0c0
robksawyer <robksawyer@gmail.com>
2014-10-22 14:59:40 -0700

    Initial commit

---
+++ api/models/User.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+module.exports = {
+    schema: true,
+
+    attributes: {
+        username: {
+            type: 'string',
+            unique: true
+        },
+        email: {
+            type: 'email',
+            unique: true
+        },
+        firstName: {
+            type: 'string',
+            required: true
+        },
+        lastName: {
+            type: 'string',
+            required: true
+        },
+        admin: {
+            type: 'boolean',
+            defaultsTo: false
+        },
+
+        // Below is all specification for relations to another models
+
+        // Passport configurations
+        passports: {
+            collection: 'Passport',
+            via: 'user'
+        },
+        // Message objects that user has sent
+        messages: {
+            collection: 'message',
+            via: 'user'
+        },
+        // Login objects that are attached to user
+        logins: {
+            collection: 'UserLogin',
+            via:        'user'
+        }
+    }
+};
